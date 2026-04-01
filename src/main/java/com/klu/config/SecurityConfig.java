5475a7dc2d81712a9bd091f3e99bc28075690808
Sunkari Himabindu <himabindusunkari16@gmail.com>
2026-04-01 15:21:48 +0530

    Update SecurityConfig.java

--- src/main/java/com/klu/config/SecurityConfig.java
+++ src/main/java/com/klu/config/SecurityConfig.java
@@ -31,7 +31,7 @@ public class SecurityConfig {
         CorsConfiguration config = new CorsConfiguration();
 
         config.setAllowedOrigins(List.of(
-            "e-commerce-frontend-production-96ab.up.railway.app"
+            "https://e-commerce-frontend-production-96ab.up.railway.app"
         ));
 
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
